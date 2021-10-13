f74b37c1c7158334e50f79cd16ab5ade6372a033
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2021-10-13 14:11:01 +0530

    initial commit

--- src/views/sideBar/sideBarComponent.js
+++ src/views/sideBar/sideBarComponent.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from  "../../assets/images/Logo.png"
-import image from  "../../assets/images/add-user.png"
-import users from  "../../assets/images/users.png"
-import weather from  "../../assets/images/weather.png"
-
+import logo from "../../assets/images/Logo.png";
+import image from "../../assets/images/add-user.png";
+import users from "../../assets/images/users.png";
+import weather from "../../assets/images/weather.png";
 
 function SideBar() {
   return (
     <div>
       <ul class="nav bg-white rounded sidebar">
         <div className="content">
-          <img
-            src={logo}
-            className="img_logo"
-            width="100"
-            height="30"
-          />
-          <hr className="my-2" style={{ backgroundColor: "#66ccff" }} />
+          {/* <img src={users} className="img_logo"  /> */}
+          {/* <hr className="my-2" style={{ backgroundColor: "#66ccff" }} /> */}
           <li class="nav-item">
             <Link to={"/add-user"} className="nav-link">
               <img src={image} />
